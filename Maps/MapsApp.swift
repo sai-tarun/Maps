11a494dd762ca403dbc8d464db3fc507179d9de0
sai-tarun <tarunsai349@gmail.com>
2025-04-11 00:08:09 +0530

    Initial Commit

---
+++ Maps/MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsApp.swift
+//  Maps
+//
+//  Created by Majji  Sai Tarun on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
