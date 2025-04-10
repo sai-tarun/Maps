6038de79a1b17c4f2eb44174301751c5b68c8373
sai-tarun <tarunsai349@gmail.com>
2025-04-11 01:03:27 +0530

    Added Content file.

--- Maps/ContentView.swift
+++ Maps/ContentView.swift
@@ -6,6 +6,89 @@
 //
 
 import SwiftUI
+import GoogleMaps
+import CoreLocation
+
+struct MapView : UIViewRepresentable {
+    
+    //Binding TotalDistance
+    @Binding var totalDistance : CLLocationDistance
+    
+//    func makeCoordinator() -> Coordinator {
+//        return Coordinatior(totalDistance: $totalDistance)
+//    }
+    
+    
+    func makeUIView(context: Context) -> GMSMapView {
+        let mapView = GMSMapView()
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        context.coordinator
+        
+        //Request Location Authorization
+       // context.coordinator.locationManager
+        //False
+        return mapView
+    }
+    func updateUIView(_ uiView: GMSMapView, context: Context) {
+        
+    }
+    
+    //Coordinator to Handle Events
+    class Coordinatior : NSObject,CLLocationManagerDelegate{
+        var mapView : GMSMapView?
+        var locationManager = CLLocationManager()
+        var previousLocation : CLLocation?
+        var polyline : GMSPolyline?
+        @Binding var totalDistance : CLLocationDistance
+        
+        init(totalDistance : Binding<CLLocationDistance>){
+            self._totalDistance = totalDistance
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let location = locations.first else {return }
+            
+            //Calculate Location and Update total Distance
+            if let previousLocation = self.previousLocation {
+                let distance = location.distance(from: previousLocation)
+                self.totalDistance += distance
+            }
+            
+            //Update Polyline to Draw the route
+            if let path = polyline?.path {
+                let path = GMSMutablePath(path: path)
+                path.add(location.coordinate)
+                polyline?.path = path
+            }else {
+                let path = GMSMutablePath()
+                path.add(location.coordinate)
+                let newPolyline = GMSPolyline(path: path)
+                newPolyline.strokeColor = .red
+                newPolyline.strokeWidth = 5.0
+                newPolyline.map = mapView
+                polyline = newPolyline
+            }
+            
+            
+            //set camera position to the Current Location
+            let zoom : Float = 18.0
+            let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: zoom)
+            mapView?.animate(to: camera)
+            self.previousLocation = location
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            if status == .authorizedWhenInUse {
+                locationManager.startUpdatingLocation()
+                mapView?.isMyLocationEnabled = true
+                mapView?.settings.myLocationButton = true
+            }
+        }
+        
+        
+    }
+}
 
 struct ContentView: View {
     var body: some View {
--- Maps/MapsApp.swift
+++ Maps/MapsApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import GoogleMaps
+import GooglePlaces
+
 
 @main
 struct MapsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate : NSObject,UIApplicationDelegate {
+    let Apikey : String = "AIzaSyD_2ZZZd1hSRwHm0dRC_E2XbqNL16-8C2k"
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        GMSServices.provideAPIKey(Apikey)
+        GMSPlacesClient.provideAPIKey(Apikey)
+        return true
     }
 }
